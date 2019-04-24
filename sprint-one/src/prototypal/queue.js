d969efc51caacb6624637a655f44ba5de24b39da
Alastair Williams <bootcamp@alwm.net>
2019-04-23 21:23:50 -0600

    Finished prototypal queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  return newQueue;
 };
 
 var queueMethods = {};
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+queueMethods.enqueue = function(value) {
+  var largest = 0;
+  for (let key in this.storage) {
+    if (key > largest) {
+      largest = key;
+    }
+  }
+  this.storage[largest + 1] = value;
+};
 
+queueMethods.dequeue = function() {
+  var smallest = Number.MAX_SAFE_INTEGER;
+  for (let key in this.storage) {
+    if (key < smallest) {
+      smallest = key;
+    }
+  }
+  let value = this.storage[smallest];
+  delete this.storage[smallest];
+  return value;
+};
 
