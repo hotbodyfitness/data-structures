bd3b2865965241c66cfa5a517f67162c9142d2b4
Alastair Williams <bootcamp@alwm.net>
2019-04-23 20:51:24 -0600

    Completed functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,30 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let largest = 0;
+    for (let key in storage) {
+      if (key > largest) {
+        largest = key;
+      }
+    }
+    storage[largest + 1] = value;
   };
 
   someInstance.pop = function() {
+    let largest = 0;
+    for (let key in storage) {
+      if (key > largest) {
+        largest = key;
+      }
+    }
+
+    let value = storage[largest];
+    delete storage[largest];
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
