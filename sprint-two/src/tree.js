7f4a8815b0384d3e08c8543ba6e0282d56eca3b8
Zack Miller <hotbodyfitness@gmail.com>
2019-04-27 12:45:11 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,36 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
   newTree.children = [];
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
+var extend = function(output, input) {
+  for(let key in input) {
+    output[key] = input[key];
+  }
+}
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-
+  this.children.push(new Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
 
+  if (target === this.value) {
+    found = true;
+  } else if (this.children.length > 0) {
+    this.children.forEach(child => {
+       found = found || child.contains(target);
+    });
+    return found;
+  }
+  return found;
 };
 
 
